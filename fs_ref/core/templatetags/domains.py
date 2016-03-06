b150ce8260cbc5ca705f9432ba043431f54ba21c
Rolf Erik Lekang <me@rolflekang.com>
2016-03-06 20:38:51 +0100

    fix: Add flag linking to correct domain

---
+++ fs_ref/core/templatetags/domains.py
@@ -0,0 +1,15 @@
+from django.template.base import Library
+
+register = Library()
+
+
+@register.simple_tag
+def domain_for_lang(language_code):
+    try:
+        return {
+            'no': 'lekang.com',
+            'se': 'filterteknik.se',
+            'dk': 'filterteknik.dk',
+        }[language_code]
+    except KeyError:
+        return ''
